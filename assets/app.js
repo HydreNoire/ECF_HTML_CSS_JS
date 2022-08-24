9af72bca4c9cb1750e3b78e7c808a33cb7fb0325
Hydre <81431125+HydreNoire@users.noreply.github.com>
2022-08-24 16:50:07 +0200

    add a message onclick btn

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
 const dog_btn = document.querySelector('#dog_btn');
 const dog_result = document.querySelector('#dog_result');
+const btn_msg = document.querySelector('.btn-msg');
+const msg = ['You like dogs ?', 'Seems you like them', 'He\'s so cute', 'You want him?', "How are you?"];
 
 dog_btn.addEventListener('click', getRandomDog);
 
@@ -8,9 +10,17 @@ function getRandomDog() {
         .then(res => res.json())
         .then(data => {
             dog_result.style.backgroundImage = "url(" + data.message + ")";
+
             dog_btn.classList.add('btn-success');
             setTimeout(() => {
                 dog_btn.classList.remove('btn-success');
             }, 500);
+
+            btn_msg.style.display = "block";
+            btn_msg.innerText = msg[Math.floor(Math.random() * msg.length)];
+            setTimeout(() => {
+                btn_msg.style.display = "none";
+                btn_msg.innerText = "";
+            }, 1000);
         });
 }
